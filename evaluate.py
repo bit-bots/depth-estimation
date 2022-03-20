450cd327c7afd8040b6ced6b3c1b28ea9f9e6bdb
Niklas Fiedler <git@nfiedler.de>
2022-03-20 20:50:27 +0100

    better fix for evaluation

--- evaluate.py
+++ evaluate.py
@@ -11,25 +11,20 @@ def evaluate(net, dataloader, device, progressbar=True, dataloader_len=None):
         num_val_batches = len(dataloader)
     else:
         num_val_batches = dataloader_len
-    dice_score = 0
+    val_score = 0
 
     # iterate over the validation set
     for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False, disable=not progressbar):
         image, mask_true = batch['image'], batch['mask']
         # move images and labels to correct device and type
         image = image.to(device=device, dtype=torch.float32)
-        mask_true = mask_true.to(device=device, dtype=torch.long) 
+        mask_true = mask_true.to(device=device, dtype=torch.float32)
 
         with torch.no_grad():
             # predict the mask
-            mask_pred = net(image)
-            mask_pred = -1/(1-(1/mask_pred)) 
+            mask_pred = net(image).clamp(max=0.99, min=0.01)
+            mask_pred = -1/(1-(1/mask_pred))
             err = torch.abs(mask_true-mask_pred)
-            dice_score += -torch.mean(err).cpu()
-            
+            val_score += -torch.mean(err).cpu() / num_val_batches
     net.train()
-
-    # Fixes a potential division by zero error
-    if num_val_batches == 0:
-        return dice_score
-    return dice_score / num_val_batches
+    return val_score
